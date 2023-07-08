68774494572dd7b33287a9e7c6860f983793da1c
carlosthrasher <carloshcpereira@outlook.com.br>
2023-07-08 15:09:10 -0300

    Iniciado o quarto desafio, iniciado a string, separada para uma lista, falta resolver o problema que não está coletando todas as strings da lista

--- challenge.py
+++ challenge.py
@@ -1,9 +1,9 @@
 #Desafio 1
 
 phrase_1 = "Hello, World! OpenAI is amazing"
-words = phrase_1.split() #dividi a frase em palavras pelo espaço criando uma lista
-words.reverse() #inverte as palavras da lista
-inverted_phrase = " ".join(words) # cria uma nova string com as palavras invertidas 
+words_1 = phrase_1.split() #dividi a frase em palavras pelo espaço criando uma lista
+words_1.reverse() #inverte as palavras da lista
+inverted_phrase = " ".join(words_1) # cria uma nova string com as palavras invertidas 
 print(inverted_phrase)
 
 
@@ -13,9 +13,21 @@ print(inverted_phrase)
 from collections import OrderedDict 
 
 phrase_2 = "Hello, World!"
-removed_duplicated_chars = "".join(OrderedDict.fromkeys(phrase_2)) # é criado um dictionary atraves do metodo fromkeys() removendo as duplicatas e refeita a string atraves do .join()
+removed_duplicated_chars = "".join(OrderedDict.fromkeys(phrase_2)) # é criado um dictionary atraves do metodo fromkeys() removendo as duplicatas e refeita a string atraves do "".join()
 
 print(removed_duplicated_chars) 
+
+
+#Desafio 4
+phrase_4 = "hello, how are you? i'm, fine, thank you."
+words_2 = phrase_4.split()
+print(words_2)
+for new_words in words_2:
+    words_capitalize = new_words.capitalize()
+    
+
+new_phrase_4 = "".join(words_capitalize)
+print(new_phrase_4)
 
 #desafio 5
 
